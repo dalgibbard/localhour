d76791f0500be51e8784df178d5cf197dd27e4c9
Darren Gibbard <dalgibbard@gmail.com>
2016-03-13 12:23:04 +0000

    Updated facter fact to use HTTPS

--- facter_example.rb
+++ facter_example.rb
@@ -1,12 +1,26 @@
-
 require 'facter'
-require 'net/http'
+require 'net/https'
+require "uri"
 require 'json'
 
-response20 = Net::HTTP.get_response("dguix.com","/timediff/" + 22)
-response1 = Net::HTTP.get_response("dguix.com","/timediff/" + 1)
+# Get 8pm in localDC time
+uri20 = URI.parse("https://dgunix.com/localhour/20")
+http20 = Net:HTTP.new(uri20.host, uri20.port)
+http20.use_ssl = true
+http20.verify_mode = OpenSSL::SSL::VERIFY_NONE
+request20 = Net::HTTP::Get.new(uri20.request_uri)
+response20 = http.request(request20)
 hour20 = JSON.parse(response20.body)['hour']
+
+# Get 1am in localDC time
+uri1 = URI.parse("https://dgunix.com/localhour/1")
+http1 = Net:HTTP.new(uri1.host, uri1.port)
+http1.use_ssl = true
+http1.verify_mode = OpenSSL::SSL::VERIFY_NONE
+request1 = Net::HTTP::Get.new(uri1.request_uri)
+response1 = http.request(request1)
 hour1 = JSON.parse(response1.body)['hour']
+
 
 Facter.add('compacthour') do
   setcode do
